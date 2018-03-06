091c8afa90fd16ba78518c99c10c6420a9bf62f0
jbjog <jeanbaptiste.jogeix@outlook.fr>
2018-03-06 09:34:34 +0100

    refactor responsive

--- src/components/ControlPanel/ControlPanel.vue
+++ src/components/ControlPanel/ControlPanel.vue
@@ -2,21 +2,23 @@
   <div id="controlpanel">
     <globalfilter></globalfilter>
     <columnfilter></columnfilter>
-    <div id="buttons">
-      <a class="button blue" download="export.json" v-bind:href="makeTextFile">Exporter</a>
-      <button id="multiple-delete-button" class="red" @click="multipleDelete">Supprimer</button>
-      <button id="show-form-intervention" class="green" @click="showFormPanel">Ajouter</button>
-      <select v-model="pageValue" @change="changePageSize">
-        <option v-for="element in pageSizeList">{{element}}</option>
-      </select>
-    </div>
-    <div id="page">
-      <button v-for="pageOcc in getPagesArray"
-              :disabled="isActivePage(pageOcc)"
-              :class="isActivePage(pageOcc)? 'activePage' : ''"
-              @click="changePageNumber(pageOcc)">
-        {{ pageOcc  }}
-      </button>
+    <div id="panel-fixed">
+      <div id="buttons">
+        <a class="button blue" download="export.json" v-bind:href="makeTextFile">Exporter</a>
+        <button id="multiple-delete-button" class="red" @click="multipleDelete">Supprimer</button>
+        <button id="show-form-intervention" class="green" @click="showFormPanel">Ajouter</button>
+        <select v-model="pageValue" @change="changePageSize">
+          <option v-for="element in pageSizeList">{{element}}</option>
+        </select>
+      </div>
+      <div id="page">
+        <button v-for="pageOcc in getPagesArray"
+                :disabled="isActivePage(pageOcc)"
+                :class="isActivePage(pageOcc)? 'activePage' : ''"
+                @click="changePageNumber(pageOcc)">
+          {{ pageOcc  }}
+        </button>
+      </div>
     </div>
     <div>
      <span> {{ getDisplayMessagePagin }}</span>
